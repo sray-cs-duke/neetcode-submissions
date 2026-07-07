6a778e83b06caf824318b45f3ca8b9b4b6019051
Siddhant Ray <siddhantrayduke@gmail.com>
2026-07-07 11:37:52 +0530

    Add: binary-search - submission-5

---
+++ Data Structures & Algorithms/binary-search/submission-5.py
@@ -0,0 +1,38 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        rows = len(matrix)
+        cols = len(matrix[0])
+        top = 0
+        bottom = rows - 1
+        while top <= bottom:
+            row = (top + bottom) // 2
+
+            if target > matrix[row][cols - 1]:
+                top = row + 1
+
+            elif target < matrix[row][0]:
+                bottom = row - 1
+
+            else:
+                break
+
+        if not (top <= bottom):
+            return False
+        row = (top + bottom) // 2
+
+        l = 0
+        r = cols - 1
+
+        while l <= r:
+            mid = (l + r) // 2
+
+            if matrix[row][mid] == target:
+                return True
+
+            elif matrix[row][mid] < target:
+                l = mid + 1
+
+            else:
+                r = mid - 1
+
+        return False
